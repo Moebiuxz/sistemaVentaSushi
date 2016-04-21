32c8d635b29afe022518b7701dd7bf01ce314758
Cero <Cero@Cero.home>
2016-04-20 23:00:55 -0300

    init_progra_DLV - Avance en clase DAO

--- src/java/bd/Conexion.java
+++ src/java/bd/Conexion.java
@@ -45,6 +45,4 @@ public class Conexion {
         resultado = sentencia.executeQuery(select);
         return resultado;
     }
-    
-    
 }
--- src/java/bd/DAO.java
+++ src/java/bd/DAO.java
@@ -5,9 +5,10 @@ import modelo.TipoUsuario;
 import modelo.Usuario;
 
 public class DAO {
+
     public final Conexion C;
     private String sql;
-    
+
     public DAO() throws SQLException {
         C = new Conexion(
                 DatosConexion.SERVER,
@@ -16,10 +17,45 @@ public class DAO {
                 DatosConexion.PASS
         );
     }
+
+    /*
+     Métodos TipoUsuario
+     */
+    public void crearTipousuario(TipoUsuario tipo) throws SQLException {
+        sql = "INSERT INTO tipoUsuario VALUES("
+                + "null,"
+                + "'" + tipo.getNombre() + "',"
+                + "'" + tipo.getEstado() + "'"
+                + ");";
+        C.ejecutar(sql);
+    }
+
+    public TipoUsuario getTipoUsuario(int id) throws SQLException {
+        TipoUsuario tipo = null;
+        sql = "SELECT * FROM tipoUsuario WHERE tipoUsuario_id = '" + id + "'";
+        C.resultado = C.ejecutarSelect(sql);
+        if (C.resultado.next()) {
+            tipo = new TipoUsuario(C.resultado.getInt(1), C.resultado.getString(2), C.resultado.getInt(3));
+        }
+        C.sentencia.close();
+        return tipo;
+    }
     
+    public void actualizarTipoUsuario(TipoUsuario tipo) throws SQLException{
+        sql = "update tipoUsuario set "
+                + "tipoUsuario_nombre = '"+tipo.getNombre()+"', "
+                + "tipoUsuario_estado = '"+tipo.getEstado()+"' "
+                + "where "
+                + "tipoUsuario_id = '"+tipo.getId()+"';";
+        C.ejecutar(sql);
+    }
+
+    /*
+     Métodos TipoUsuario
+     */
     public Usuario getUsuario(Usuario u) throws SQLException {
         Usuario user = null;
-        sql = "SELECT * FROM usuario WHERE usuario_nombre = '" + u.getNombre()+ "' AND usuario_clave = AES_ENCRYPT('"+u.getPassword()+"',666)";
+        sql = "SELECT * FROM usuario WHERE usuario_nombre = '" + u.getNombre() + "' AND usuario_clave = AES_ENCRYPT('" + u.getPassword() + "',666)";
         C.resultado = C.ejecutarSelect(sql);
         if (C.resultado.next()) {
             user = new Usuario(
@@ -33,16 +69,5 @@ public class DAO {
         C.sentencia.close();
         System.out.println(u.getNombre());
         return user;
-    }
-    
-    public TipoUsuario getTipoUsuario(int id) throws SQLException{
-        TipoUsuario tipo = null;
-        sql = "SELECT * FROM tipoUsuario WHERE tipoUsuario_id = '"+id+"'";
-        C.resultado = C.ejecutarSelect(sql);
-        if (C.resultado.next()) {
-            tipo = new TipoUsuario(C.resultado.getInt(1), C.resultado.getString(2), C.resultado.getInt(3));
-        }
-        C.sentencia.close();
-        return tipo;
     }
 }
