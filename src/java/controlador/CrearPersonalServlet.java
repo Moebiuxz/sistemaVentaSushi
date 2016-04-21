1606f9b7488206f401aacf39ec24c39d71aa0ae4
LAB-315 <LAB-315@LAB-315-PC>
2016-04-21 14:08:37 -0300

    En esta revision se creó el crud de personal.

---
+++ src/java/controlador/CrearPersonalServlet.java
@@ -0,0 +1,83 @@
+package controlador;
+
+import bd.DAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import modelo.Personal;
+
+@WebServlet(name = "CrearPersonal", urlPatterns = {"/crearPersonal.do"})
+public class CrearPersonalServlet extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        
+        try {
+            String nombre, rut, apellido, tipo;
+            int tipofin;
+            
+            rut = request.getParameter("txtRut");
+            nombre = request.getParameter("txtNombre");
+            apellido = request.getParameter("txtApellido");
+            tipo = request.getParameter("cboTipoProducto");
+            tipofin = Integer.parseInt(tipo);
+            
+            Personal p = new Personal(1, rut, nombre, apellido, tipofin, 1, 1);
+            DAO dd = new DAO();
+            
+            dd.crearPersonal(p);
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(CrearPersonalServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
